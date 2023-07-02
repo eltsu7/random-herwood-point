a65b1afad0b9a78e53220beb8744d48ae1f6f3bb
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2023-07-03 00:02:57 +0300

    asdasdasdasd

--- draw.py
+++ draw.py
@@ -12,7 +12,7 @@ def draw_on_map(
     save_image: bool = False,
     plot_title: str = "",
     image_name: str = "",
-    annonate: bool = True,
+    annotate: bool = True,
 ):
     map_image = plt.imread(map_name)
     aspect_ratio = (
@@ -25,15 +25,21 @@ def draw_on_map(
     fig: plt.Figure
     fig, ax = plt.subplots(figsize=(12, 8))
 
-    x = [point[0] for point in map_points]
-    y = [point[1] for point in map_points]
-    ax.plot(x, y, zorder=1, alpha=0.5, c="b")
+    ax.plot(
+        [point[0] for point in map_points],
+        [point[1] for point in map_points],
+        zorder=1,
+        alpha=0.5,
+        c="b",
+    )
 
     for point in points:
         ax.scatter(point.x, point.y, zorder=1, alpha=1, c="r", s=20)
 
     ax.set_title(plot_title, loc="left", fontsize=20)
-    ax.set_title(datetime.datetime.now(), loc="right", fontsize=10)
+    ax.set_title(
+        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), loc="right", fontsize=10
+    )
     ax.set_xlim(MapLimits.lon_min, MapLimits.lon_max)
     ax.set_ylim(MapLimits.lat_min, MapLimits.lat_max)
     ax.imshow(
@@ -48,7 +54,7 @@ def draw_on_map(
         aspect=aspect_ratio,
     )
 
-    if annonate:
+    if annotate:
         for point in points:
             ax.annotate(int(point.z), (point.x + 0.0003, point.y + 0.0003), fontsize=14)
 
@@ -80,9 +86,11 @@ def draw_on_map(
         )
 
     plt.axis("off")
+
     if save_image:
         if image_name == "":
             raise NameError("Filename is empty")
         fig.savefig(image_name, bbox_inches="tight", pad_inches=0.2, dpi=200)
+
     if show_image:
         plt.show()
--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from datetime import datetime
 
 
 week_number = datetime.now().isocalendar()[1]
-filename = f"images\\week_{week_number}asdasd"
+filename = f"images\\week_{week_number}_{int(datetime.now().timestamp())}"
 title = f"Viikko {week_number}"
 
-points = generate_random_points(5)
+points = generate_random_points(10)
 for i, point in enumerate(points):
     print(f"{i + 1}: https://google.com/maps/search/?api=1&query={point.y},{point.x}")
 
-draw_on_map(points, save_image=True, image_name=filename, plot_title=title)
+draw_on_map(
+    points,
+    show_image=True,
+    save_image=False,
+    image_name=filename,
+    plot_title=title,
+    annotate=True,
+)
