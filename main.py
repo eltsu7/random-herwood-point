ac3a3d15342c754cc26d936bb44be9900dcca0dd
Eeli Hernesniemi <eeli.hernesniemi@optofidelity>
2023-07-10 09:39:20 +0300

    count over 5 for next week

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import os.path
-import sys
+
+import shapely
 
 from random_points import generate_random_points
 from draw import draw_on_map
 from datetime import datetime
 
+point_week_number: int = (datetime.now() - datetime(year=2023, month=7, day=3)).days // 7
+calendar_week_number: int = datetime.now().isocalendar()[1]
 
-week_number = datetime.now().isocalendar()[1]
 file_names = [
-    os.path.join("images", f"week_{week_number}_{int(datetime.now().timestamp())}"),
+    os.path.join("images", f"week_{calendar_week_number}_{int(datetime.now().timestamp())}"),
     os.path.join("images", f"latest"),
 ]
 
-title = f"Viikon {week_number} kuvausrastit"
-
-points = generate_random_points(5)
+title = f"Viikon {calendar_week_number} kuvausrastit"
+points = generate_random_points(number_of_points=5, week_offset=point_week_number)
 
 draw_on_map(
     points,
     show_image=True,
-    save_files=False,
+    save_files=True,
     file_names=file_names,
     plot_title=title,
     annotate=True,
--- random_points.py
+++ random_points.py
@@ -4,7 +4,7 @@ import numpy as np
 from map_properties import map_points, list_of_holes
 
 
-def generate_random_points(number_of_points: int) -> list[Point]:
+def generate_random_points(number_of_points: int, week_offset: int) -> list[Point]:
     map_polygon = Polygon(map_points, holes=list_of_holes)
     boundaries = map_polygon.bounds
 
@@ -13,7 +13,8 @@ def generate_random_points(number_of_points: int) -> list[Point]:
     while len(points) < number_of_points:
         x = round(np.random.uniform(boundaries[0], boundaries[2]), 4)
         y = round(np.random.uniform(boundaries[1], boundaries[3]), 4)
-        point = Point(x, y, len(points) + 1)
+        z = len(points) + 1 + (number_of_points * week_offset)
+        point = Point(x, y, z)
         if map_polygon.contains(point):
             points.append(point)
 
